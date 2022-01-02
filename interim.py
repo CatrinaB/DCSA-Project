4975c2c3b92afa90b7f74f25143e5221402bb808
CatrinaB <catrina.bodean@yahoo.com>
2022-01-02 23:46:12 +0100

    Task 1 roughly completed
    Task 1 finds top 10 keywords
    TO DO:
    - fine-tune for titles with quotation marks inside
    - first line removal
    - investigate 'titl'

---
+++ interim.py
@@ -0,0 +1,32 @@
+import nltk
+
+str = "B*A*P*S (1997),Comedy"
+
+text = nltk.word_tokenize(str)
+# print(nltk.pos_tag(text, tagset='universal'))
+# print(nltk.pos_tag(text))
+
+tagged = nltk.pos_tag(text, 'universal')
+
+print(tagged)
+
+print("drama".split('|'))
+#
+# remove_pos = ['ADP', 'ADV', 'CONJ', 'DET', 'PRT', '.', 'NUM']
+# final = ''
+#
+# for i in tagged:
+#     if i[1] not in remove_pos:
+#         final += (i[0] + ' ')
+#
+# print(final[0:-2])
+#
+# # aa = 'ana'
+# # bb = 'ma'
+# #
+# # print(''.join())
+
+# with open('movies_mini.csv', 'r') as inf:
+#     for line in inf:
+#         if(line.find('"') > -1):
+#             print(line.split('"'))
---
+++ task1.py
@@ -0,0 +1,77 @@
+import nltk
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+# WORD_RE = re.compile(r"[\w']+")  # match words
+
+
+class MovieGenreKeywords(MRJob):
+
+    def mapper(self, _, line):
+        line = line.strip().lower()
+        if line.find('"') > -1:
+            words = line.split('"')
+            words[2] = words[2][1:]
+        else:
+            words = line.split(',')
+
+        genres = words[2].split('|')
+
+        for genre in genres:
+            yield genre, self.remove_title_words(words[1])
+
+    def combiner(self, genre, title):
+        yield genre, ','.join(title)
+
+    def reducer_title_string(self, genre, title):
+        yield genre, ','.join(title)
+
+    def mapper_2(self, genre, string):
+        words = string.split(' ')
+        for word in words:
+            yield genre, (word, 1)
+
+    def combiner_2(self, genre, lista):
+        word_list = {}
+        for i in lista:
+            if i[0] in word_list:
+                word_list[i[0]] += i[1]
+            else:
+                word_list[i[0]] = i[1]
+
+        yield genre, word_list
+
+    def reducer_2(self, genre, words):
+        dct = next(words)
+        yield genre, sorted(dct.items(), key=lambda el: el[1], reverse=True)[:10]
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   combiner=self.combiner,
+                   reducer=self.reducer_title_string),
+            MRStep(mapper=self.mapper_2,
+                   combiner=self.combiner_2,
+                   reducer=self.reducer_2)
+        ]
+
+    def remove_title_words(self, title):
+        title = title[:title.find('(')]
+        text = nltk.word_tokenize(title)
+
+        tagged = nltk.pos_tag(text, 'universal')
+
+        remove_pos = ['ADP', 'ADV', 'CONJ', 'DET', 'PRT', '.', 'NUM']
+        remove_words = ['a.k.a', '*', 'ii']
+        final = ''
+
+        for i in tagged:
+            if i[1] not in remove_pos and i[0] not in remove_words:
+                final += (i[0] + ' ')
+
+        return final[0:-1]
+
+
+if __name__ == '__main__':
+    MovieGenreKeywords.run()
